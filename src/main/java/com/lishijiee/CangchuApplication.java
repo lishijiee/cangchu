350b3f88d0653a1e0ce1b03db6f13d6ec8134e43
lishijiee <2424782003@qq.com>
2022-04-21 08:34:51 +0800

    Initial commit

---
+++ src/main/java/com/lishijiee/CangchuApplication.java
@@ -0,0 +1,13 @@
+package com.lishijiee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CangchuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CangchuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lishijiee/CangchuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishijiee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CangchuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
