14ae70b1ee9d5320db0be01c156d0f8135967a9f
lishijiee <2424782003@qq.com>
2022-04-28 21:47:24 +0800

    不指定扫描路径也可以

--- src/main/java/com/lishijiee/CangchuApplication.java
+++ src/main/java/com/lishijiee/CangchuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lishijiee.mapper")
+//@MapperScan("com.lishijiee.mapper")
 public class CangchuApplication {
 
     public static void main(String[] args) {
